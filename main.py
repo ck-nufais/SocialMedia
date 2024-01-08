564b4305926f3549317e417c2cf702e4c0851e7e
ck-nufais <ck.nufiii@gmail.com>
2024-01-08 23:14:31 +0530

    added a model for followers

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ migrate = Migrate(app,db)
 
 import models,routes 
 
-from models import Users
+from models import Users,Follow
 @app.shell_context_processor
 def make_shell_context():
-    return {'sql': sa, 'orm': so, 'db': db, 'User': Users}
+    return {'sql': sa, 'orm': so, 'db': db, 'User': Users,"Follow":Follow}
 
 
--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from main import db,Login
-from sqlalchemy import String,ForeignKey
+from sqlalchemy import String,ForeignKey,select,func
 from sqlalchemy.orm import relationship,Mapped,mapped_column,WriteOnlyMapped
 from werkzeug.security import generate_password_hash ,check_password_hash
 from flask_login import UserMixin
 from datetime import datetime,timezone
 
 class Follow(db.Model):
-    id:Mapped[int] = mapped_column(primary_key=True)
+    # id:Mapped[int] = mapped_column(primary_key=True)
     follower_id:Mapped[int] = mapped_column(ForeignKey("users.id"),primary_key=True)
-    following_id:Mapped[int] = mapped_column(ForeignKey("users.id"),primary_key=True)
+    followed_id:Mapped[int] = mapped_column(ForeignKey("users.id"),primary_key=True)
 
 
 
@@ -18,15 +18,30 @@ class Users(UserMixin,db.Model):
     email:Mapped[str] = mapped_column(String(60),index=True,unique=True)
     password:Mapped[str]=mapped_column(String(256))
     posts:WriteOnlyMapped['Posts'] = relationship(back_populates="user")
-    followers:WriteOnlyMapped["Users"] = relationship(back_populates="follows"    ,secondary=Follow,secondaryjoin=( id==Follow.following_id),primaryjoin=(Follow.follower_id==id))
-    follows:WriteOnlyMapped["Users"]= relationship(back_populates="followers",
-        secondary=Follow, primaryjoin=(Follow.followed_id == id), secondaryjoin=(Follow.follower_id == id))
-
+    followers:WriteOnlyMapped["Users"] = relationship(back_populates="follows" ,secondary='follow',secondaryjoin=( id==Follow.followed_id),primaryjoin=(Follow.follower_id==id))
+    follows:WriteOnlyMapped["Users"]= relationship(back_populates="followers", secondary='follow', primaryjoin=(Follow.followed_id == id), secondaryjoin=(Follow.follower_id == id))
     def generate_hash(self,password):
         self.password = generate_password_hash(password)
     def check_hash(self,password):
         return check_password_hash(self.password,password)
+    def follow(self,user):
+        if not self.is_following(user):
+            self.follows.add(user)
+    def unfollow(self,user):
+        if self.is_following(user):
+            self.follows.remove(user)
 
+    def following_count(self):
+        query = select(func.count()).select_from(
+            self.follows.select().subquery())
+        return db.session.scalar(query)
+    def is_following(self ,user):
+        q = self.follows.select().where(Users.id == user.id)
+        return db.session.scalar(q)
+    def followers_count(self):
+        query = select(func.count()).select_from(
+            self.followers.select().subquery())
+        return db.session.scalar(query)
 
 
 
